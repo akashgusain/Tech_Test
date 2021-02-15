113b8aaba7c9264aecbe1fd816f1a1da7d8fbf0c
Akash Gusain <akash.gusain.ext@nokia.com>
2021-02-16 02:59:54 +0530

    create folder and made changes for CSV Upload

--- teacher_db/models.py
+++ teacher_db/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Subject(models.Model):
-    subject = models.CharField(max_length=20, null=False, blank=False)
+    subject = models.CharField(max_length=20, null=False, blank=False, unique=True)
 
     def __str__(self):
         return self.subject
@@ -15,7 +15,7 @@ class Subject(models.Model):
 class Teacher(models.Model):
     first_name = models.CharField(max_length=50, blank=False, null=False)
     last_name = models.CharField(max_length=10, blank=True, null=True)
-    image = models.ImageField(upload_to='media/', default='/media/default.jpg')
+    image = models.ImageField(upload_to='', default='default.jpg')
     email_id = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=False, null=False)
     room_no = models.CharField(max_length=5)
--- teacher_db/views.py
+++ teacher_db/views.py
@@ -2,20 +2,22 @@ import csv
 import io
 
 from django.core.files import File
+from rest_framework import viewsets, filters, status
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
-
-from rest_framework import viewsets, filters, status
-from .serializers import TeacherSerializer, UploadSerializer, SubjectSerializer
-from .models import Teacher, Subject
 from rest_framework.response import Response
+
+from .models import Teacher, Subject
+from .serializers import TeacherSerializer, UploadSerializer, SubjectSerializer
 
 
 class TeacherViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing teacher instances.
     """
+    parser_classes = (MultiPartParser,)
     serializer_class = TeacherSerializer
     queryset = Teacher.objects.all()
     filter_backends = [filters.SearchFilter]
@@ -41,13 +43,15 @@ class UploadViewSet(viewsets.ViewSet):
         decoded_file = csv_file.read().decode('utf-8')
         reader = csv.reader(io.StringIO(decoded_file), delimiter=',')
         next(reader, None)
+        data_list = []
 
         for row in reader:
-            data = {}
+            teacher_dict = {}
             """Image not found , will take default image"""
             try:
-                image_file = File(open('media/' + row[2], 'rb'))
-                data.update({"image": image_file})
+                image_file = File(open('teacher_image/' + row[2], 'rb'))
+
+                teacher_dict.update({"image": image_file})
             except IOError:
                 pass
 
@@ -55,22 +59,24 @@ class UploadViewSet(viewsets.ViewSet):
 
             if len(subjects) > 5:
                 return Response(
-                    row[0] + " " + row[1] + "A teacher can teach no more than 5 subjects, Please Check Excel sheet",
+                    row[0] + " " + row[1] + " A teacher can teach no more than 5 subjects, Please Check Excel sheet",
                     status=status.HTTP_400_BAD_REQUEST)
             else:
                 try:
-                    sublist = [Subject.objects.get(subject__startswith=sub).id for sub in subjects]
-                except Exception:
+                    sublist = [Subject.objects.get(subject__istartswith=sub).id for sub in subjects]
+                except Exception as ex:
                     return Response("Subjects listed in Excel File, Not present in DB",
                                     status=status.HTTP_400_BAD_REQUEST)
 
-            data.update({"first_name": row[0], "last_name": row[1],
-                         "email_id": row[3], "phone": row[4], "room_no": row[5], "subject": sublist})
+            teacher_dict.update({"first_name": row[0], "last_name": row[1],
+                                 "email_id": row[3], "phone": row[4], "room_no": row[5], "subject": sublist})
 
-            serializer = TeacherSerializer(data=data)
+            data_list.append(teacher_dict)
 
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = TeacherSerializer(data=data_list, many=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
